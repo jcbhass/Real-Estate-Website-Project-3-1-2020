3d0ccecba69d5f86222e8aee63bf987f39e85b89
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-03-09 18:47:07 -0400

    installed HtmlWebPackPlugin

--- src/server/server.js
+++ src/server/server.js
@@ -9,4 +9,9 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.static('dist'));
+app.use(express.static('dist'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
